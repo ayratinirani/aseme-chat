9d97f88a54772068345a058bc08a400e818e55ed
mohammadounegh <mohammadounegh@gmailcom>
2020-03-28 13:09:15 +0430

    added layouting and some testing for template and layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',"users":[
+    "ali","سعیده","uhwli"
+  ]});
 });
 router.get('/aseme', function(req, res, next) {
   res.render('index', { title: 'عاصمه قیز' });
